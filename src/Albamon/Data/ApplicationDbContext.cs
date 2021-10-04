0af57e95a4d926303c166255b136a1069a375024
Pedro Jesús Martínez Herrero <PedroJesus.Martinez1@alu.uclm.es>
2021-10-05 00:34:57 +0200

    Incluidas clases en 'ApplicationDbContext.cs'

--- src/Albamon/Data/ApplicationDbContext.cs
+++ src/Albamon/Data/ApplicationDbContext.cs
@@ -4,13 +4,20 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Albamon.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Albamon.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public DbSet<NFT> NFT { get; set; }
         public DbSet<TypeNFT> TypeNFT { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<NFT> NFT { get; set; }
+        public DbSet<Wallet> Wallet { get; set; }
+        public DbSet<Venta> Venta { get; set; }
+        public DbSet<VentaItem> VentaItem { get; set; }
+
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
