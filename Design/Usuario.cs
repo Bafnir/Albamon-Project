b2554b442e8fc16ef5efe649d2ba1c6610a14580
Pedro Jesús Martínez Herrero <PedroJesus.Martinez1@alu.uclm.es>
2021-10-04 23:49:36 +0200

    Creada la clase 'Usuario'

---
+++ Design/Usuario.cs
@@ -0,0 +1,32 @@
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+namespace Albamon.Models
+{
+    public class Usuario : IdentityUser
+    {
+        public int Nombre
+        {
+            get;
+            set;
+            
+        }
+
+        public int Apellidos
+        {
+            get;
+            set;
+           
+        }
+
+        public int DNI
+        {
+            get;
+            set;
+            
+        }
+    }
+}
