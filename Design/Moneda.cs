988320cfc0018599f36559d98a2aeb4f938d2b60
PEDRO MIÑANO VARGAS <Pedro.Minano@alu.uclm.es>
2021-10-05 00:47:07 +0200

    Creada clase "Moneda.cs"

---
+++ Design/Moneda.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design
+{
+    class Moneda
+    {
+        public virtual int TokenId
+        {
+            get;
+            set;
+        }
+        [Range(1, int.MaxValue, ErrorMessage = "No posee la cantidad minima de 1 para comprar")]
+        public virtual int CantidadCompra
+        {
+            get;
+            set;
+        }
+        [Range(1, int.MaxValue, ErrorMessage = "Minima de cantidad de 100 para vender")]
+        public virtual int CantidadVenta
+        {
+            get;
+            set;
+        }
+    }
+}
