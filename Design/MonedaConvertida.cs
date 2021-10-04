7284b9810fb4810f25bb8e15b3e18e7b29b81807
PEDRO MIÑANO VARGAS <Pedro.Minano@alu.uclm.es>
2021-10-05 01:57:43 +0200

    Creada "MonedaConvertida"

---
+++ Design/MonedaConvertida.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Design
+{
+    class MonedaConvertida
+    {
+        [Key]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        [ForeignKey("MonedaId")]
+        public virtual Moneda Moneda
+        {
+            get;
+            set;
+        }
+
+        [Range(1, Double.MaxValue, ErrorMessage = "Cantidad insuficiente de monedas")]
+        public virtual int Cantidad
+        {
+            get;
+            set;
+        }
+
+        public virtual int monedaId
+        {
+            get;
+            set;
+        }
+        [ForeignKey("ConversionId")]
+        public virtual Conversion Conversion
+        {
+            get;
+            set;
+        }
+
+        public virtual int ConversionId
+        {
+            get;
+            set;
+        }
+    }
+}
