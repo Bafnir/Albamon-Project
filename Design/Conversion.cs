7f76d1be2a12ed547b55017ff41ab79715fb1a19
PEDRO MIÑANO VARGAS <Pedro.Minano@alu.uclm.es>
2021-10-05 01:47:59 +0200

    Creada la clase "Conversion.cs"

--- Design/Conversion.cs
+++ Design/Conversion.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Design
 {
     public class Conversion
     {
+        [Key]
+        public int ConversionId
+        {
+            get;
+            set;
+        }
+
+
+
+        public DateTime FechaConversion
+        {
+            get;
+            set;
+        }
+
+
+        public virtual IList<MonedaConvertida> MonedasConvertidas
+        {
+            get;
+            set;
+        }
+        public Conversion()
+        {
+
+            MonedasConvertidas = new List<MonedaConvertida>();
+        }
+        [ForeignKey("ClienteId")]
+        public virtual Cliente Cliente
+        {
+            get;
+            set;
+        }
+
+        
+        public string ClienteId
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "Wallet")]
+        [Required()]
+        public Wallet Wallet
+        {
+            get;
+            set;
+        }
     }
 }
