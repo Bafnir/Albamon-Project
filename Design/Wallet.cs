08ad39590a538ccc36dc86cffdf57d3d7d115314
Pedro Jesús Martínez Herrero <PedroJesus.Martinez1@alu.uclm.es>
2021-10-04 23:48:58 +0200

    Creada la clase 'Wallet'

---
+++ Design/Wallet.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Albamon.Models
+{
+    public class Wallet
+    {
+        [Key]
+        public int Dirección
+        {
+            get;
+            set;
+            
+        }
+        public int Saldo
+        {
+            get;
+            set;
+
+        }
+
+        public void Equals()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
