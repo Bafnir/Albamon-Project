82a2a1ed31cab8fe8c205548ab4104a997c20cd1
PEDRO MIÑANO VARGAS <Pedro.Minano@alu.uclm.es>
2021-10-05 02:31:58 +0200

    Modificada la clase ApplicationDbContext

--- src/Albamon/Data/ApplicationDbContext.cs
+++ src/Albamon/Data/ApplicationDbContext.cs
@@ -4,13 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Albamon.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Albamon.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
-        public DbSet<NFT> NFT { get; set; }
-        public DbSet<TypeNFT> TypeNFT { get; set; }
+        public DbSet<Admin> Admin { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Conversion> Conversion { get; set; }
+        public DbSet<Moneda> Moneda { get; set; }
+        public DbSet<MonedaConvertida> MonedaConvertida { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Wallet> Wallet{ get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
