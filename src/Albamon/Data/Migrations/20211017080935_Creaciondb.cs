1d37950895174dd314d4d8fa7fc9c448cdd5e3be
ALEJANDRO PÉREZ MARTÍNEZ <Alejandro.Perez22@alu.uclm.es>
2021-10-17 10:15:26 +0200

    Arreglar master 3

---
+++ src/Albamon/Data/Migrations/20211017080935_Creaciondb.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Albamon.Data.Migrations
+{
+    public partial class Creaciondb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Wallet",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Purchase",
+                columns: table => new
+                {
+                    PurchaseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TotalPrice = table.Column<double>(type: "float", nullable: false),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Purchase", x => x.PurchaseId);
+                    table.ForeignKey(
+                        name: "FK_Purchase_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TypeNFT",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tier = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypeNFT", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NFT",
+                columns: table => new
+                {
+                    NftId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Health = table.Column<double>(type: "float", nullable: false),
+                    Attack = table.Column<double>(type: "float", nullable: false),
+                    Rarity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TypeNFTId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NFT", x => x.NftId);
+                    table.ForeignKey(
+                        name: "FK_NFT_TypeNFT_TypeNFTId",
+                        column: x => x.TypeNFTId,
+                        principalTable: "TypeNFT",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationUserNFT",
+                columns: table => new
+                {
+                    ApplicationUsersId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    NFTSNftId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUserNFT", x => new { x.ApplicationUsersId, x.NFTSNftId });
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserNFT_AspNetUsers_ApplicationUsersId",
+                        column: x => x.ApplicationUsersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserNFT_NFT_NFTSNftId",
+                        column: x => x.NFTSNftId,
+                        principalTable: "NFT",
+                        principalColumn: "NftId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PurchaseNFTS",
+                columns: table => new
+                {
+                    NftId = table.Column<int>(type: "int", nullable: false),
+                    PurchaseId = table.Column<int>(type: "int", nullable: false),
+                    Pid = table.Column<int>(type: "int", nullable: false),
+                    Fee = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PurchaseNFTS", x => new { x.NftId, x.PurchaseId });
+                    table.ForeignKey(
+                        name: "FK_PurchaseNFTS_NFT_NftId",
+                        column: x => x.NftId,
+                        principalTable: "NFT",
+                        principalColumn: "NftId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PurchaseNFTS_Purchase_PurchaseId",
+                        column: x => x.PurchaseId,
+                        principalTable: "Purchase",
+                        principalColumn: "PurchaseId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserNFT_NFTSNftId",
+                table: "ApplicationUserNFT",
+                column: "NFTSNftId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NFT_TypeNFTId",
+                table: "NFT",
+                column: "TypeNFTId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchase_ApplicationUserId",
+                table: "Purchase",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseNFTS_PurchaseId",
+                table: "PurchaseNFTS",
+                column: "PurchaseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationUserNFT");
+
+            migrationBuilder.DropTable(
+                name: "PurchaseNFTS");
+
+            migrationBuilder.DropTable(
+                name: "NFT");
+
+            migrationBuilder.DropTable(
+                name: "Purchase");
+
+            migrationBuilder.DropTable(
+                name: "TypeNFT");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Wallet",
+                table: "AspNetUsers");
+        }
+    }
+}
