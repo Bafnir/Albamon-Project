c59ed4e2a866622cd1625e198269e9fbee78b86a
Pedro Jesús Martínez Herrero <PedroJesus.Martinez1@alu.uclm.es>
2021-10-19 12:56:27 +0200

    Creado directorio CU VenderNFT con las clases

---
+++ Design/CU VenderNFT/Cliente.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+namespace Design
+{
+    public class Cliente : Usuario
+    {
+
+        public virtual IList<Venta> Ventas { get; set; }
+        public virtual IList<Conversion> Conversiones
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ Design/CU VenderNFT/Venta.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Design
+{
+    public class Venta
+
+    {
+        public Venta()
+        {
+            VentaItems = new List<VentaItem>();
+        }
+
+        public virtual int Precio
+        {
+            get;
+            set;
+        }
+
+        public virtual int ID
+        {
+            get;
+            set;
+        }
+
+        public virtual int Wallet_Signature
+        {
+            get;
+            set;
+        }
+        [Key]
+        public virtual int VentaID
+        {
+            get;
+            set;
+        }
+        public virtual string ClienteID
+        {
+            get;
+            set;
+        }
+
+
+        public virtual Wallet Wallet
+        {
+            get;
+            set;
+        }
+        [ForeignKey("ClienteId")]
+        public virtual Cliente Cliente
+        {
+            get;
+            set;
+        }
+        [NotMapped]
+        public virtual IList<VentaItem> VentaItems
+        {
+            get;
+            set;
+        }
+
+     
+    }
+
+}
