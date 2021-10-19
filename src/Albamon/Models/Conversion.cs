46289913322d5e94bbeb425c850cb0b503547122
PEDRO MIÑANO VARGAS <Pedro.Minano@alu.uclm.es>
2021-10-19 20:34:35 +0200

    Migración y base de datos hecha

--- src/Albamon/Models/Conversion.cs
+++ src/Albamon/Models/Conversion.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Design
+namespace Albamon.Models
 {
     public class Conversion
     {
--- src/Albamon/Models/Moneda.cs
+++ src/Albamon/Models/Moneda.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Design
+namespace Albamon.Models
 {
     public class Moneda
     {
