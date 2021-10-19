ee8458e0a0afb8496126a95a5c19df1e3f34ec28
PEDRO MIÑANO VARGAS <Pedro.Minano@alu.uclm.es>
2021-10-19 20:28:18 +0200

    Añadidas las clases a Model

---
+++ src/Albamon/Models/Conversion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Design
+{
+    public class Conversion
+    {
+        [Key]
+        public int ConversionId
+        {
+            get;
+            set;
+        }
+
+
+
+        public DateTime FechaConversion
+        {
+            get;
+            set;
+        }
+
+
+        public virtual IList<MonedaConvertida> MonedasConvertidas
+        {
+            get;
+            set;
+        }
+        public Conversion()
+        {
+
+            MonedasConvertidas = new List<MonedaConvertida>();
+        }
+        [ForeignKey("ClienteId")]
+        public virtual Cliente Cliente
+        {
+            get;
+            set;
+        }
+
+        
+        public string ClienteId
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "Wallet")]
+        [Required()]
+        public Wallet Wallet
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ src/Albamon/Models/Moneda.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design
+{
+    public class Moneda
+    {
+        [Key]
+        public virtual int MonedaId
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(10, ErrorMessage = "Nombre de la moneda")]
+        public virtual String Nombre
+        {
+            get;
+            set;
+        }
+       
+        [Range(1, int.MaxValue, ErrorMessage = "No posee la cantidad minima de 1 para comprar")]
+        public virtual int CantidadCompra
+        {
+            get;
+            set;
+        }
+        [Range(100, int.MaxValue, ErrorMessage = "Minima de cantidad de 100 para vender")]
+        public virtual int CantidadVenta
+        {
+            get;
+            set;
+        }
+        public virtual IList<MonedaConvertida> MonedasConvertidas
+        {
+            get;
+            set;
+        }
+    }
+}
