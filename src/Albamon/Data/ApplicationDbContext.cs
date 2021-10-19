c375ac6969a64293fa75d79b0d06396830f91a9a
Pedro Jesús Martínez Herrero <PedroJesus.Martinez1@alu.uclm.es>
2021-10-19 13:37:42 +0200

    Añadidas clases a ApplicationDbContext.cs

--- src/Albamon/Data/ApplicationDbContext.cs
+++ src/Albamon/Data/ApplicationDbContext.cs
@@ -10,11 +10,18 @@ namespace Albamon.Data
 {
     public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
+        
         public DbSet<NFT> NFT { get; set; }
         public DbSet<TypeNFT> TypeNFT { get; set; }
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
         public DbSet<Purchase> Purchase { get; set; }
         public DbSet<PurchaseNFT> PurchaseNFTS { get; set; }
+        public DbSet<Admin> Admin { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<VentaItem> VentaItems { get; set; }
+        public DbSet<Wallet> Wallet { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
