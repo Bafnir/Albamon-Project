d3432f85dc90038fc8635873a73621eebed22757
ALEJANDRO PÉREZ MARTÍNEZ <Alejandro.Perez22@alu.uclm.es>
2021-11-01 00:51:38 +0100

    Añadir seeddata

---
+++ dbo.TypeNFT.data.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT [dbo].[TypeNFT] ON
+INSERT INTO [dbo].[TypeNFT] ([Id], [Description], [Name], [Tier]) VALUES (3, N'FIRE', N'FIRE', 2)
+INSERT INTO [dbo].[TypeNFT] ([Id], [Description], [Name], [Tier]) VALUES (4, N'WATER', N'WATER', 1)
+INSERT INTO [dbo].[TypeNFT] ([Id], [Description], [Name], [Tier]) VALUES (5, N'LIGHT', N'LIGHT', 2)
+INSERT INTO [dbo].[TypeNFT] ([Id], [Description], [Name], [Tier]) VALUES (6, N'PLANT', N'PLANT', 2)
+SET IDENTITY_INSERT [dbo].[TypeNFT] OFF
