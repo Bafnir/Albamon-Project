3fcf455ae311e6f019f83fd69a0df47c4d4b649d
Pedro Jesús Martínez Herrero <PedroJesus.Martinez1@alu.uclm.es>
2021-11-02 17:59:20 +0100

    View SelectForVenta

--- src/Albamon/Controllers/VentasController.cs
+++ src/Albamon/Controllers/VentasController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Albamon.Data;
 using Albamon.Models;
+using Albamon.Models.NFTViewModels;
 
 namespace Albamon.Controllers
 {
